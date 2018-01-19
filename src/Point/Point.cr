b6bd5b3c43b4b69b705563d032198922dc04bf69
sombrastudios <nilsmartel@concepts-system.com>
2018-01-19 20:43:31 +0100

    Add a bunch of Methods for Point

--- src/Point/Point.cr
+++ src/Point/Point.cr
@@ -6,5 +6,21 @@ struct Point
 	def initialize(@x, @y)
 	end
 
+	def +(p : Point) : Point
+		return new x+p.x, y+p.y
+ 	end
+
+	def -(p : Point) : Point
+		return new x-p.x, y-p.y
+ 	end
+
+	def *(s) : Point
+		return new x*s, y*s
+	end
+
+	def abs() : Point
+		return new x.abs, y.abs
+	end
+
 	# TODO add Bunch of Methods for Subtracting, Adding etc.
 end
