2085525d3bc8285a637f5d19f0d9fbebdd519eb2
sombrastudios <nilsmartel@concepts-system.com>
2018-01-24 14:47:58 +0100

    Add alternative Constructor for Example Widget

--- src/UI/Example.cr
+++ src/UI/Example.cr
@@ -6,7 +6,11 @@ module Glass
 		@width = 64_u32
 		@height= 64_u32
 
-		def initialize(@image, @background_color : SF::Color)
+		def initialize(@background_color : SF::Color)
+		end
+
+		def initialize(r, g, b : UInt8)
+			@background_color = SF::Color.new r, g, b
 		end
 	end
 end
