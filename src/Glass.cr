39aafad091ffcdabe03263bf01498c16438d04f3
sombrastudios <nilsmartel@concepts-system.com>
2018-02-01 06:15:52 +0100

    Remove redundant Code

--- src/Glass.cr
+++ src/Glass.cr
@@ -34,60 +34,11 @@ def new_ui() : Glass::Widget
 	img = SF::Image.new(256, 256)
 	ui = Glass::AbsolutContainer.new(img)
 	ui.background_color = SF::Color.new(64_u8, 64_u8, 64_u8)
-	# doesn't fucking work
 	container = Glass::VerticalContainer.new()
 	container + Glass::Example.new(128_u8, 64_u8, 32_u8)
 	container + Glass::Example.new(32_u8, 128_u8, 64_u8)
 	container + Glass::Example.new(64_u8, 32_u8, 128_u8)
 	ui + container
-
-	# fake_render Glass::ImageClip.new img
-
-	# This Method most definitly Calls all it's children render functions
-	# it still does not fucking work
 	ui.render()
 	ui
-end
-
-def fake_render(img : Glass::ImageClip)
-	# So, this shit works. Nice
-	# So far ImageClip doesn't seem to be the Problem
-	(0..255_u8).each do |x|
-		(0..255_u8).each do |y|
-			if (x*y) % 4 == 0
-				img.set_pixel x, y, SF::Color.new(x, y, 255_u8)
-			end
-		end
-	end
-
-	clip = img.get_clip(
-		Glass::Point.new(64, 64),
-		128_u32,
-		128_u32
-	)
-
-	(0...128_u8).each do |x|
-		(0...128_u8).each do |y|
-			clip.set_pixel x, y, SF::Color.new(255 - y, 255 - x, 128_u8)
-		end
-	end
-
-	# TODO
-	# HAH, this shit doesn't even work!
-	#
-	# Found Bug??!
-	#
-	# Set's the Clip Relative to the raw (0, 0) Coordinates of the main Image,
-	# not relative to clips position!
-	clip2 = clip.get_clip(
-		Glass::Point.new(120, 16),
-		32_u32,
-		32_u32
-	)
-
-	(0...32_u8).each do |x|
-		(0...32_u8).each do |y|
-			clip2.set_pixel x, y, SF::Color.new(32_u8, 255_u8, 128_u8)
-		end
-	end
 end
